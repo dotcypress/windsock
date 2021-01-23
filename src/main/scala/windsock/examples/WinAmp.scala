7aa01b2c8a94e68dfdb75ee53574154e37991aa5
Vitaly Domnikov <dotcypress@gmail.com>
2021-01-23 00:00:08 -0800

    PCM

---
+++ src/main/scala/windsock/examples/WinAmp.scala
@@ -0,0 +1,56 @@
+package windsock.examples
+
+import java.io.FileInputStream
+import spinal.core._
+import spinal.lib._
+import spinal.lib.graphic.Rgb
+import spinal.lib.misc._
+import windsock.bsp._
+import windsock.lib.ui._
+import windsock.lib.pmods._
+
+object WinAmp {
+  def main(args: Array[String]) = ECPIX5.generate(new WinAmp)
+}
+
+case class WinAmp() extends Component {
+  val io = new Bundle {
+    val pmod4 = master(AudioAmp())
+    val pmod7 = master(SnapOff())
+    val leds = out(LedArray())
+  }
+
+  io.leds.switchOff()
+
+  val snapOff = new SnapOffCtrl
+  snapOff.io.pins <> io.pmod7
+
+  val amp = new AudioAmpCtrl()
+  amp.io.pins <> io.pmod4
+  amp.io.enable := snapOff.io.button1
+  amp.io.gain := snapOff.io.button2
+
+  val soundPath = "src/main/resources/winamp.raw"
+  val sampleRate = 20800 Hz
+
+  val ram = Mem(
+    Bits(8 bits),
+    wordCount = new FileInputStream(soundPath).available()
+  )
+  BinTools.initRam(ram, soundPath)
+
+  val track = new SlowArea(sampleRate) {
+    val frame = ram.readSync(
+      enable = True,
+      address = Counter(ram.wordCount, snapOff.io.button1)
+    )
+
+    snapOff.io.led1 := frame(7) & frame(5)
+    snapOff.io.led2 := frame(7) & frame(4)
+    snapOff.io.led3 := frame(7) & frame(3)
+    snapOff.io.led4 := frame(7) & frame(2)
+    snapOff.io.led5 := frame(7) & frame(1)
+
+    amp.io.sample := U(frame)
+  }
+}
---
+++ src/main/scala/windsock/lib/pmods/AudioAmp.scala
@@ -0,0 +1,55 @@
+package windsock.lib.pmods
+
+import spinal.core._
+import spinal.lib._
+
+case class AudioAmp(lowConnector: Boolean = true) extends PMODBundle {
+  override def asMaster() = {
+    val value = if (lowConnector) {
+      out(pin7, pin8, pin10)
+      in(pin1, pin2, pin3, pin4, pin9)
+    } else {
+      out(pin1, pin2, pin4)
+      in(pin3, pin7, pin8, pin9, pin10)
+    }
+  }
+}
+
+case class AudioAmpCtrl(lowConnector: Boolean = true) extends Component {
+  val io = new Bundle {
+    val pins = master(AudioAmp(lowConnector))
+    val sample = in(UInt(8 bits))
+    val enable = in(Bool())
+    val gain = in(Bool())
+  }
+
+  val pdm = PDM(8 bit)
+  pdm.io.input <> io.sample
+
+  if (lowConnector) {
+    io.pins.pin7 := B(pdm.io.output)
+    io.pins.pin8 := ~B(io.gain)
+    io.pins.pin10 := B(io.enable)
+  } else {
+    io.pins.pin1 := B(pdm.io.output)
+    io.pins.pin2 := ~B(io.gain)
+    io.pins.pin4 := B(io.enable)
+  }
+}
+
+case class PDM(width: BitCount) extends Component {
+  val io = new Bundle {
+    val input = in(UInt(width))
+    val output = out(Bool())
+  }
+
+  val max = UInt(width).setAll()
+  val acc = Reg(UInt(width.value + 1 bit))
+  acc := acc + io.input
+
+  io.output := False
+  when(acc >= max) {
+    acc := acc - max
+    io.output := True
+  }
+}
